fe35dd1f863d614b2d9345f5cf6068e037b369c8
ForsakenDelusion <wangcy0205@outlook.com>
2024-04-03 11:58:25 +0800

    save

---
+++ 24.Swap Nodes in Pairs/24.Swap Nodes in Pairs-java/24.swap-nodes-in-pairs.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=java
+ * @lcpr version=30121
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode sentinel;
+        ListNode nextNode;
+        ListNode curNode;
+        
+
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
