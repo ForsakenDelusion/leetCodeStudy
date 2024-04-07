cb1d76f217a299638a2b4a19bb2961c00eb2f68e
ForsakenDelusion <wangcy0205@outlook.com>
2024-04-07 18:56:57 +0800

    两两交换链表中的元素

--- 24.Swap Nodes in Pairs/24.Swap Nodes in Pairs-java/24.swap-nodes-in-pairs.java
+++ 24.Swap Nodes in Pairs/24.Swap Nodes in Pairs-java/24.swap-nodes-in-pairs.java
@@ -5,7 +5,6 @@
  * [24] 两两交换链表中的节点
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
@@ -13,60 +12,44 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode sentinel;
-        ListNode nextNode;
-        ListNode curNode;
-        ListNode tempHead;
         ListNode temp;
-        ListNode tempTail;
-
-        sentinel = new ListNode(205, head);
-        curNode = sentinel;
-        temp = head.next;
-        nextNode = head;
-
-        while (temp != null && temp.next != null) {
+        ListNode tempNext;
+        ListNode sentinel = new ListNode(205,head);
+        ListNode curNode = sentinel;
+        
+        while (curNode.next != null && curNode.next.next != null) {
+            temp = curNode.next.next;
+            tempNext = curNode.next.next.next;
+            curNode.next.next.next = curNode.next;
+            curNode.next.next = tempNext;
             curNode.next = temp;
-            curNode = temp;
-            curNode.next = nextNode;
-            nextNode.next = curNode.next;
-            nextNode = curNode.next;
-            temp = nextNode.next;
-            
+            curNode = curNode.next.next;
         }
-
-
-        
-        
-
         return sentinel.next;
     }
 }
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// [1,2,3,4]\n
-// @lcpr case=end
-
-// @lcpr case=start
-// []\n
-// @lcpr case=end
-
-// @lcpr case=start
-// [1]\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // [1,2,3,4]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // []\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1]\n
+ * // @lcpr case=end
+ * 
  */
-
