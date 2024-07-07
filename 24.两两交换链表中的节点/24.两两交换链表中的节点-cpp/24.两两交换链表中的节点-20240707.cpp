bc4f8e2f19b96bb9ddc56f985ffc234abf408c02
ForsakenDelusion <wangcy0205@outlook.com>
2024-07-07 17:48:51 +0800

    update 24.sawp pairs of node in Linked List by Cpp

---
+++ 24.两两交换链表中的节点/24.两两交换链表中的节点-cpp/24.两两交换链表中的节点-20240707.cpp
@@ -0,0 +1,78 @@
+// @lcpr-before-debug-begin
+
+// @lcpr-before-debug-end
+
+/*
+ * @lc app=leetcode.cn id=24 lang=cpp
+ * @lcpr version=30204
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        ListNode *sentinelNode = new ListNode(0);
+        sentinelNode->next = head;
+        ListNode *temp;
+        ListNode *curNode = sentinelNode;
+        ListNode *nextNode;
+        while (curNode->next != NULL && curNode->next->next != NULL)
+        {
+            nextNode = curNode->next;
+            temp = nextNode->next;
+            curNode->next = temp;
+            nextNode->next = temp->next;
+            temp->next = nextNode;
+            curNode = nextNode;
+        }
+        return sentinelNode->next;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
