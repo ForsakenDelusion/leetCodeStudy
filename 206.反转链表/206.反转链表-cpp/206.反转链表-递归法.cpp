9369c4dd86b45cd37a41c6fe2751c53fbd685803
Forsaken Delusion <2430385640cy@gmail.com>
2024-07-07 15:35:15 +0800

    update 206.reverse list by cpp using Recursion method

---
+++ 206.反转链表/206.反转链表-cpp/206.反转链表-递归法.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ * @lcpr version=30204
+ *
+ * [206] 反转链表
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverse(ListNode *pre, ListNode *cur)
+    {
+        if (cur == NULL)
+        {
+            return pre;
+        }
+        ListNode *temp = cur->next;
+        cur->next = pre;
+        return reverse(cur, temp);
+    }
+
+    ListNode *reverseList(ListNode *head)
+    {
+        return reverse(NULL, head);
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
