6a428d1b4f7d2dce3013cb37bb090b48048008c0
ForsakenDelusion <wangcy0205@outlook.com>
2024-07-13 22:06:03 +0800

    add 15.three nums sum by Cpp

--- 15.三数之和/15.三数之和-cpp/15.三数之和-20240710.cpp
+++ 15.三数之和/15.三数之和-cpp/15.三数之和-20240710.cpp
@@ -1,3 +1,10 @@
+// @lcpr-before-debug-begin
+
+
+
+
+// @lcpr-before-debug-end
+
 /*
  * @lc app=leetcode.cn id=15 lang=cpp
  * @lcpr version=30204
@@ -6,15 +13,48 @@
  */
 
 // @lc code=start
+#include <algorithm>
+#include <cstddef>
+#include <unordered_set>
+#include <vector>
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        
+  vector<vector<int>> threeSum(vector<int> &nums) {
+    int sum = 0;
+    std::vector<std::vector<int>> res;
+    std::sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] > 0) {
+        return res;
+      }
+      if (i > 0 && nums[i] == nums[i - 1]) { // 剪枝函数
+        continue;
+      }
+      int left = i + 1;
+      int right = nums.size() - 1;
+      while (left < right) {
+        sum = nums[i] + nums[left] + nums[right];
+        if (sum > 0) {
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          res.push_back(std::vector<int>{nums[i], nums[left], nums[right]});
+          while (left < right && nums[left] == nums[left + 1]) {
+            left++;
+          }
+          while (left < right && nums[right] == nums[right - 1]) {
+            right--;
+          }
+          right--;
+          left++;
+        }
+      }
     }
+    return res;
+  }
 };
 // @lc code=end
-
-
 
 /*
 // @lcpr case=start
@@ -30,4 +70,3 @@ public:
 // @lcpr case=end
 
  */
-
