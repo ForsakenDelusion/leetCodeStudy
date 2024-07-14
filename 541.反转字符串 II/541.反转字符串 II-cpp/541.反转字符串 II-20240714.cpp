7ae4034a5c1d5e67c71a94f9201083c1dc3b71cf
Forsaken Delusion <2430385640cy@gmail.com>
2024-07-14 09:52:56 +0800

    add solution of reverse str by Cpp

---
+++ 541.反转字符串 II/541.反转字符串 II-cpp/541.反转字符串 II-20240714.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=541 lang=cpp
+ * @lcpr version=30204
+ *
+ * [541] 反转字符串 II
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+  string reverseStr(string s, int k) {}
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "abcdefg"\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abcd"\n2\n
+// @lcpr case=end
+
+ */
