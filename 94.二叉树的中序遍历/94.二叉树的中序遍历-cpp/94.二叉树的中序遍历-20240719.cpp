711be39bb933fa10693142d287b875f90109449a
Forsaken Delusion <2430385640cy@gmail.com>
2024-07-19 21:39:02 +0800

    add 94 by Cpp

---
+++ 94.二叉树的中序遍历/94.二叉树的中序遍历-cpp/94.二叉树的中序遍历-20240719.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ * @lcpr version=30204
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lcpr-template-start
+#include <fstream>
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  void traversal(TreeNode *cur, vector<int>& res) {
+    if (cur == NULL) {
+      return;
+    }
+    traversal(cur->left, res);
+    res.push_back(cur->val);
+    traversal(cur->right, res);
+  }
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> res;
+    traversal(root, res);
+    return res;
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
