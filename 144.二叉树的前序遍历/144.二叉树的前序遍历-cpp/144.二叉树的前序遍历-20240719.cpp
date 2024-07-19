ce76d0fc583fa4932c3e1cd8b6f98c21ff04b0d5
Forsaken Delusion <2430385640cy@gmail.com>
2024-07-19 15:39:43 +0800

    add 144.Preorder traversal of binary tree by Cpp

---
+++ 144.二叉树的前序遍历/144.二叉树的前序遍历-cpp/144.二叉树的前序遍历-20240719.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=cpp
+ * @lcpr version=30204
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lcpr-template-start
+#include <cstddef>
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<int> res;
+  void traversal(TreeNode *cur, vector<int> &res) {
+    if (cur == NULL) {
+      return;
+    }
+    res.push_back(cur->val);
+    traversal(cur->left, res);
+    traversal(cur->right, res);
+    
+  }
+  vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> res;
+    traversal(root, res);
+    return res;
+  }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,null,2]\n
+// @lcpr case=end
+
+ */
