39d93eb5ecaadb75440e0a2429d29c77f1aaf022
Forsaken Delusion <2430385640cy@gmail.com>
2024-08-06 21:27:47 +0800

    fix 106 style

---
+++ 654.最大二叉树/654.最大二叉树-cpp/654.最大二叉树-20240806.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=cpp
+ * @lcpr version=30204
+ *
+ * [654] 最大二叉树
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [3,2,1,6,0,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,1]\n
+// @lcpr case=end
+
+ */
+
